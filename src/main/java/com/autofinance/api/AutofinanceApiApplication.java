81c01cda69f9d10407c1f5c4b766842ed5f96a84
njlmrvllr <anjaliamarovillar@outlook.com>
2026-06-24 01:28:39 -0500

    chore: initial commit

---
+++ src/main/java/com/autofinance/api/AutofinanceApiApplication.java
@@ -0,0 +1,13 @@
+package com.autofinance.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutofinanceApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutofinanceApiApplication.class, args);
+    }
+
+}
